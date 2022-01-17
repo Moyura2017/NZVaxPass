80ca6f30c17052e625dbc216fe4bdbee495d31d4
Moyura2017 <moyura2017@gmail.com>
2022-01-17 20:44:08 +1300

    Updated

--- js/nzvaxpass.js
+++ js/nzvaxpass.js
@@ -1,3 +1,14 @@
 export function decoder(qrCode) {
-  console.log('Decoder: ' + qrCode);
+  // qrCode='';
+  // throw 500
+  
+  if (qrCode.length == 0) throw new Error('Empty Payload');
+  var payload = qrCode.split('/');
+  //console.log('var: ' + (payload[1]) );
+
+  if (payload[0] != 'NZCP:') throw new Error('Invalid payload prefix');
+  if(payload[1] != 1) throw new Error('Invalid payload version');
+  payload=payload[2];
+
+  console.log(payload);
 }
